448588693a58957614dd0c37d63882fc8b5657ed
cjulian <carlo.julian@gmail.com>
2014-02-19 02:51:31 -0500

    Adding grounded check with raycast

---
+++ Assets/Scripts/CameraControl.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraControl : MonoBehaviour {
+
+	public GameObject player = null;
+	float offsetX = 0;
+	float offsetY = 0;
+
+	// Use this for initialization
+	void Start () {
+		offsetX = this.transform.position.x;
+		offsetY = this.transform.position.y;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		this.transform.position = new Vector3(player.transform.position.x + offsetX, offsetY, this.transform.position.z);
+	}
+}
--- Assets/Scripts/PlayerControl.cs
+++ Assets/Scripts/PlayerControl.cs
@@ -9,12 +9,15 @@ public class PlayerControl : MonoBehaviour {
 	// store input
 	private SimpleTouch touch;
 
+	// check for grounded
+	private bool grounded = false;
+
 	// JUMP related vars
 	public float jumpVel = 20;
 	public bool canGlide = false;
 	public float glideVel = -1;
-	public int maxNumJumps = 2;
-	private int numJumps = 0;
+	public int maxAirJumps = 1;
+	private int numAirJumps = 0;
 
 
 	// Use this for initialization
@@ -26,13 +29,22 @@ public class PlayerControl : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		touch = GetTouchInput ();
+		grounded = GetGroundedState();
+		label.text = grounded.ToString();
+
+		if (grounded) {
+			numAirJumps = 0;
+		}
 
 		// JUMP
 		switch (touch.touchPhase){
 			case TouchPhase.Began:
-				if (numJumps < maxNumJumps - 1) {
-					this.rigidbody.velocity = Vector3.up * jumpVel;
-					numJumps++;
+				if (grounded || numAirJumps < maxAirJumps) {
+					SetVelY(jumpVel);
+					
+					if (!grounded) {
+						numAirJumps++;
+					}
 				}
 				break;
 
@@ -40,15 +52,14 @@ public class PlayerControl : MonoBehaviour {
 			case TouchPhase.Stationary:
 			case TouchPhase.Moved:
 				if (canGlide && this.rigidbody.velocity.y <= glideVel) {
-					this.rigidbody.velocity = new Vector3(this.rigidbody.velocity.x, glideVel, this.rigidbody.velocity.z);
+					SetVelY(glideVel);
 				}
 				break;
 
 
 			case TouchPhase.Ended:
 				if (this.rigidbody.velocity.y > 0) {
-					//this.rigidbody.velocity = Vector3.zero;
-					this.rigidbody.velocity = new Vector3(this.rigidbody.velocity.x, this.rigidbody.velocity.y * 0.2f, this.rigidbody.velocity.z);
+					SetVelY(this.rigidbody.velocity.y * 0.2f);
 				}
 				break;
 
@@ -56,17 +67,42 @@ public class PlayerControl : MonoBehaviour {
 			default:				
 				break;
 		}
-
-
-		if (this.transform.position.y <= 0.5) {
-			this.transform.position = new Vector3(this.transform.position.x, 0.5f, this.transform.position.z);
-			numJumps = 0;
-		}
 	}
 
+	// TODO: limit raycast to only ground objects (check tag)
+	bool GetGroundedState()	{
+		bool grounded = false;
+		Vector3 pos = this.rigidbody.collider.bounds.center;
+		float height = this.rigidbody.collider.bounds.size.y;
+		float leftX = pos.x - this.rigidbody.collider.bounds.size.x / 2;
+		float rightX = pos.x + this.rigidbody.collider.bounds.size.x / 2;
+
+		RaycastHit hit;
+
+		// test left, right and center of collider
+		if (Physics.Raycast(new Vector3(leftX,pos.y, pos.z), -Vector3.up, out hit, height/2) ||
+		    Physics.Raycast(new Vector3(pos.x,pos.y, pos.z), -Vector3.up, out hit, height/2) ||
+		    Physics.Raycast(new Vector3(rightX,pos.y, pos.z), -Vector3.up, out hit, height/2))
+		{
+			grounded = true;
+		}
+
+		return grounded;
+	}
+
+	void SetVelX(float velX) {
+		this.rigidbody.velocity = new Vector3(velX, this.rigidbody.velocity.y, this.rigidbody.velocity.z);
+	}
+	void SetVelY(float velY) {
+		this.rigidbody.velocity = new Vector3(this.rigidbody.velocity.x, velY, this.rigidbody.velocity.z);
+	}
+	void SetVelZ(float velZ) {
+		this.rigidbody.velocity = new Vector3(this.rigidbody.velocity.x, this.rigidbody.velocity.y, velZ);
+	}
 
 	// Return touch input if touchscreen or convert mouse input to touch input
-	SimpleTouch GetTouchInput() {
+	SimpleTouch GetTouchInput()
+	{
 		bool touched = false;
 		float tx = 0;
 		float ty = 0;
