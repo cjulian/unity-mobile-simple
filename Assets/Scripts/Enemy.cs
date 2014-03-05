acce9c217f98526af1ee5e74f71d8259f8b0735a
cjulian <carlo.julian@gmail.com>
2014-03-04 20:41:03 -0500

    - Tint enemy texture when hit - Show particle explosion when hit

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -5,6 +5,7 @@ public class Enemy : MonoBehaviour {
 
 	public int healthMax = 100;
 	public int healthCurr = 100;
+	public ParticleSystem ps;
 
 	// Use this for initialization
 	void Start () {
@@ -18,7 +19,21 @@ public class Enemy : MonoBehaviour {
 		}
 	}
 
-	public void Hit(int damage) {
+	public void Hit(int damage, Vector3 point, Vector3 normal) {
+		if (ps != null) {
+			ps.transform.position = point;
+
+			ps.transform.rotation = Quaternion.LookRotation(normal);
+			ps.Emit(3);
+		}
+
 		healthCurr -= damage;
+		renderer.material.SetColor("_Color", new Color(1f, 0.9f, 0.9f));
+		StartCoroutine(DefaultColor());
+	}
+
+	private IEnumerator DefaultColor() {
+		yield return new WaitForSeconds(0.1f);
+		renderer.material.SetColor("_Color", Color.white);
 	}
 }
